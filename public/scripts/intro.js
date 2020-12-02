f884fd449176531df042b145f437cb508e10d1c5
edesiojnr <edesiojnr@gmail.com>
2020-12-02 16:11:39 -0300

    Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,70 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+//let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+//const s1 = "Isso é uma string"
+//const s2 = 'Isso também é um string'
+//const s3 = `Isso é uma string também`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidade
+//const pessoa = {
+  //  altura: "1,80m",
+ //   idade: 24,
+   // solteiro: true,
+    //correr(){
+     // document.write("executar uma grade logica de corrrer")
+   // }
+ // }
+  
+ // document.write(pessoa.correr())
+  
+  //Array - Vetores
+   // const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}] 
+   
+   //document.write(colecaoDeBolinhas[3].name)
+  
+  
+   //funções - Funcionalidades - Atalhos - Reuso de código
+
+  //registrar função
+  //function sayMyName(name) {
+
+   // document.write(name)
+  // }
+  
+  //executar
+  //sayMyName("Marina")
+  //sayMyName("Syang")
+  //sayMyName("Merlin")
+
+  //condicionais
+  
+  //const notaFinal = 7
+  
+  //if( notaFinal < 5 ) {
+   // document.write("Reprovado")
+  //} else {
+   // document.write("Aprovado")
+  // }
+  
+  // loop repetições
+  //for (i = 0; i < 10; i++) {
+  // document.write(`<p>${i}</p>`)
+  // }
+  
